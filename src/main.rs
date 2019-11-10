8301939a34188b0afb071dd11a3f11a2d5c0096b
wotmshuaisi <wotmshuaisi@users.noreply.github.com>
2019-11-10 15:10:30 +0000

    learning - actix-web - day1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use actix_web::get;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use listenfd::ListenFd;
+
+fn index() -> impl Responder {
+    return HttpResponse::Ok().body("hello world!");
+}
+
+fn index2() -> impl Responder {
+    return HttpResponse::Ok().body("hello world again!");
+}
+
+#[get("/hello")]
+fn index3() -> impl Responder {
+    return HttpResponse::Ok().body("hello world third time!");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut listen_fd = ListenFd::from_env();
+    let mut server = HttpServer::new(|| {
+        App::new()
+            .service(index3)
+            .route("/", web::get().to(index))
+            .route("/again", web::get().to(index2))
+    });
+
+    server = if let Some(l) = listen_fd.take_tcp_listener(0).unwrap() {
+        server.listen(l).unwrap()
+    } else {
+        server.bind("localhost:8000").unwrap()
+    };
+
+    server.run().unwrap();
 }
