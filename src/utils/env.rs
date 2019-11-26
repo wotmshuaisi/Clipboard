f8d6f8b23219f6a00e57e86a70758733d1d7a09c
wotmshuaisi <wotmshuaisi@users.noreply.github.com>
2019-11-26 14:59:50 +0000

    function get_env implemented

--- src/utils/env.rs
+++ src/utils/env.rs
@@ -1,5 +1,5 @@
 pub fn get_env(key: &str, default: &str) -> String {
-    match std::env::var("MODE") {
+    match std::env::var(key) {
         Ok(val) => String::from(val),
         Err(_) => String::from(default),
     }
