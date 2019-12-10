36410e2be9748a0d6ad57b288aead366a5c30c9d
wotmshuaisi <16408269+wotmshuaisi@users.noreply.github.com>
2019-12-10 10:52:15 +0000

    json object for set_clipboard

--- src/api/api_clipboard.rs
+++ src/api/api_clipboard.rs
@@ -1,9 +1,16 @@
-use actix_web::{HttpRequest, HttpResponse};
+use actix_web::{web, HttpRequest, HttpResponse};
+use bytes::{Bytes, BytesMut};
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
-struct set_clipboard_req {}
+pub struct SetClipboardReq {
+    pub content: Option<String>,
+    pub cliptype: Option<i32>,
+    pub onetime: Option<bool>,
+    pub password: Option<String>,
+    pub expire_date: Option<i64>,
+}
 
-pub async fn set_clipboard(req: HttpRequest) -> HttpResponse {
+pub async fn set_clipboard(item: web::Json<SetClipboardReq>) -> HttpResponse {
     HttpResponse::Ok().body("ok.")
 }
